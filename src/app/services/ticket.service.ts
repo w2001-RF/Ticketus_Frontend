c8f3c61879c71d6877b5e7ac3e2381a1a936c3a7
W2001-RF <rifaywassim@gmail.com>
2024-10-17 11:23:00 +0100

    add tickets services

---
+++ src/app/services/ticket.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Ticket } from '../models/ticket.model';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TicketService {
+  private apiUrl: string;
+  private endPointAuthentication: string = "Tickets"
+
+  constructor(private http: HttpClient) { 
+    this.apiUrl = environment.backendUrl;
+  }
+
+  getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('authToken');
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
+
+  getTickets(): Observable<Ticket[]> {
+    return this.http.get<Ticket[]>(this.apiUrl, { headers: this.getHeaders() });
+  }
+
+  createTicket(ticket: Ticket): Observable<Ticket> {
+    return this.http.post<Ticket>(this.apiUrl, ticket, { headers: this.getHeaders() });
+  }
+
+  updateTicket(id: number, ticket: Ticket): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, ticket, { headers: this.getHeaders() });
+  }
+
+  deleteTicket(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+  }
+  
+}
