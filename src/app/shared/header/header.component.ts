64fd6b35df564e95f2360903753323db629e31bb
W2001-RF <rifaywassim@gmail.com>
2024-10-17 12:16:01 +0100

    add header and footer component

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent {
+
+  constructor(private authService: AuthenticationService, private router: Router) {}
+
+  isAuthenticated(): boolean {
+    return this.authService.getToken() !== null;
+  }
+
+  logout(): void {
+    this.authService.clearToken();
+    this.router.navigate(['/auth/login']);
+  }
+}
