a2afdc409329849209dd4f97e5e92be7e93d7b55
W2001-RF <rifaywassim@gmail.com>
2024-10-17 22:11:20 +0100

    correct the ticket routes

--- src/app/tickets/tickets-routing.module.ts
+++ src/app/tickets/tickets-routing.module.ts
@@ -4,9 +4,9 @@ import { TicketListComponent } from './ticket-list/ticket-list.component';
 import { TicketFormComponent } from './ticket-form/ticket-form.component';
 
 const routes: Routes = [
-  { path: 'tickets', component: TicketListComponent },
-  { path: 'tickets/create', component: TicketFormComponent },
-  { path: 'tickets/edit/:id', component: TicketFormComponent }
+  { path: '', component: TicketListComponent },
+  { path: 'create', component: TicketFormComponent },
+  { path: 'edit/:id', component: TicketFormComponent }
 ];
 
 export const rout = RouterModule.forChild(routes);
