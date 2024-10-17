693970b4e5a84d008b6e2ac104624e747043bae8
W2001-RF <rifaywassim@gmail.com>
2024-10-17 12:03:36 +0100

    add auth guard to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -8,7 +9,8 @@ const routes: Routes = [
   },
   {
     path: 'tickets',
-    loadChildren: () => import('./tickets/tickets.module').then(m => m.TicketsModule)
+    loadChildren: () => import('./tickets/tickets.module').then(m => m.TicketsModule),
+    canActivate: [AuthGuard]
   },
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/auth/login' }
