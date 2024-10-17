38331bc3f685a8194f7be4510a7fd1bb2ef342ce
W2001-RF <rifaywassim@gmail.com>
2024-10-17 11:04:14 +0100

    create authentication services

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private apiUrl: string;
+  private endPointAuthentication: string = "Authentication"
+
+  constructor(private http: HttpClient) { 
+    this.apiUrl = environment.backendUrl;
+  }
+
+  login(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
+
+  signup(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/signup`, data);
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('authToken', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+  
+}
